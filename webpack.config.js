8d007634c7a8ca633fcc686732738391d6211f32
Fırat Yedibela <139630229+firatyedibela@users.noreply.github.com>
2024-05-07 17:22:45 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    open: true,
+    hot: true,
+    watchFiles: {
+      paths: ['./src/*.html'],
+    },
+  },
+};
