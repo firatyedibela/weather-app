33cb8e5c972ffd2a9321d942dc20b4f7b072eb5f
Firat Yedibela <yedibelafirat0@gmail.com>
2024-05-07 23:00:04 +0300

    Implement domUI module
    Create domUI module with an IIFE and handle the methods addListeners, handleSubmit, handleError, renderErrorMessage

---
+++ src/dom.js
@@ -0,0 +1,57 @@
+import weatherAPI from './api.js';
+
+const domUI = (function () {
+  function addListeners() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const input = document.querySelector('#location');
+    const location = input.value;
+
+    try {
+      const data = await weatherAPI.get(location);
+      if (data.error) {
+        handleError(data.error.code);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleError(code) {
+    switch (code) {
+      case 1003:
+        renderErrorMessage('Please enter a location name.');
+        break;
+      case 1006:
+        renderErrorMessage('Location not found.');
+        break;
+    }
+  }
+
+  function renderErrorMessage(msg) {
+    const prevError = document.querySelector('.location-error');
+    if (prevError) {
+      prevError.remove();
+    }
+
+    const error = document.createElement('div');
+    error.className = 'location-error';
+    error.textContent = msg;
+    document.querySelector('#form-container').appendChild(error);
+  }
+
+  function init() {
+    addListeners();
+  }
+
+  return {
+    init,
+  };
+})();
+
+export default domUI;
