39bd08a8e6c9fa23e9f65ed4938e93b616e084e5
Firat Yedibela <yedibelafirat0@gmail.com>
2024-05-09 19:28:30 +0300

    Implement renderBackgroundImage

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,6 @@
 import weatherAPI from './api.js';
-import { errorHandler } from './error.js';
+import dayJPG from './assets/images/day2.jpg';
+import nightJPG from './assets/images/night.jpg';
 
 const domUI = (function () {
   function addListeners() {
@@ -10,28 +11,61 @@ const domUI = (function () {
   async function handleSubmit(e) {
     e.preventDefault();
 
+    // Get location
     const input = document.querySelector('#location');
     const location = input.value;
 
+    // Get data
     try {
       const data = await weatherAPI.get(location);
-      console.log(data);
+      if (data) {
+        input.value = '';
+        removeErrorMessage();
+        renderContents(data);
+      }
     } catch (err) {
       console.log('An error occured: ', err);
     }
   }
 
+  function renderContents(data) {
+    console.log(
+      'IM RENDERING CONTENTS, YOU WILL SEE A LOT OF WeATHER INFO ',
+      data
+    );
+    renderBackgroundImage(data);
+    // renderPrimaryContent(data.current);
+    // renderSecondaryContent();
+    // renderForecast()
+  }
+
+  function renderBackgroundImage(data) {
+    const day = data.current.is_day;
+    if (day) {
+      document.body.style.backgroundImage = `url(${dayJPG})`;
+    } else {
+      document.body.style.backgroundImage = `url(${nightJPG})`;
+    }
+  }
+
+  function renderPrimaryContent(data) {
+    console.log("HEY I GET PRIMARY CONTENT AND I'M RENDERING IT", data);
+  }
+
   function renderErrorMessage(msg) {
+    removeErrorMessage();
+    const error = document.createElement('div');
+    error.className = 'location-error';
+    error.textContent = msg;
+    document.querySelector('#form-container').appendChild(error);
+  }
+
+  function removeErrorMessage() {
     // If an error rendered before, remove it first
     const prevError = document.querySelector('.location-error');
     if (prevError) {
       prevError.remove();
     }
-
-    const error = document.createElement('div');
-    error.className = 'location-error';
-    error.textContent = msg;
-    document.querySelector('#form-container').appendChild(error);
   }
 
   function init() {
@@ -39,6 +73,7 @@ const domUI = (function () {
   }
 
   return {
+    renderContents,
     renderErrorMessage,
     init,
   };
